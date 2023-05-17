5bed5f393a0231282121aa726a2814f5fa476442
Anzchen <Anthonyzchen@yahoo.com>
2023-05-16 21:25:00 -0400

    rain effects & color/animation fixes

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ function PageTransitions() {
         if (id) {
             //Remove selected from other buttons
             sectBtns.forEach((btn) => {
-                btn.classList.remove('active');
+                btn.classList.remove('active-btn');
             })
-            e.target.classList.add('active');
+            e.target.classList.add('active-btn');
 
             //Hide other sections
             sections.forEach((section) => {
@@ -40,5 +40,43 @@ function PageTransitions() {
         element.classList.toggle('light-mode');
     })
 }
+
+var makeItRain = function() {
+    //Clear out everything
+    rain = document.querySelector('.rain').innerHTML;
+    while (rain.firstChild) {
+        rain.removeChild(rain.firstChild);
+    }
+
+    sectRain = document.querySelectorAll('.rain');
+    console.log(sectRain);
+  
+    var increment = 0;
+    var drops = "";
+    var backDrops = "";
+  
+    while (increment < 100) {
+      //couple random numbers to use for various randomizations
+      //random number between 98 and 1
+      var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+      //random number between 5 and 2
+      var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+      //increment
+      increment += randoFiver;
+      //add in a new raindrop with various randomizations to certain CSS properties
+      drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+      backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+    }
+
+    sectFrontRain = document.querySelectorAll('.rain.front-row');
+    sectBackRain = document.querySelectorAll('.rain.back-row');
+
+    for (let i = 0; i < sectFrontRain.length; i++) {
+        sectFrontRain[i].innerHTML += drops;
+        sectBackRain[i].innerHTML += backDrops;
+    }
+  }
+  
+  makeItRain();
 
 PageTransitions();
